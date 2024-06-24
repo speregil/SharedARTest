ceb0fdfd5abcb9f2f246fa0fdc98a9bbac21025d
Sebastian <s.gilparga@gmail.com>
2024-06-24 20:32:49 +1200

    Still working in the AR

--- Assets/Scripts/Game/UI/StartGameAR.cs
+++ Assets/Scripts/Game/UI/StartGameAR.cs
@@ -1,6 +1,8 @@
 using Niantic.Lightship.SharedAR.Colocalization;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,22 +21,94 @@ public class StartGameAR : MonoBehaviour
     private string roomName = "TestRoom";
     private bool isHost;
 
+    public static event Action OnStartSharedSpaceHost;
+    public static event Action OnJoinSharedSpaceClient;
+    public static event Action OnStartSharedSpace;
+    public static event Action OnStartGame;
+
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
         spaceManager.sharedSpaceManagerStateChanged += SharedSpaceManagerOnSharedSpaceManagerStateChanged;
 
+        startGameButton.onClick.AddListener(StartGame);
+        createRoomButton.onClick.AddListener(CreateRoom);
+        joinRoomButton.onClick.AddListener(JoinGameClient);
+
+        startGameButton.interactable = false;
     }
 
     private void SharedSpaceManagerOnSharedSpaceManagerStateChanged(SharedSpaceManager.SharedSpaceManagerStateChangeEventArgs obj)
     {
         if (obj.Tracking)
         {
-
+            startGameButton.interactable = true;
+            createRoomButton.interactable = false;
+            joinRoomButton.interactable = false;
         }
     }
-    // Start is called before the first frame update
-    void Start()
+
+    void StartGame()
+    {
+        OnStartGame?.Invoke();
+
+        if(isHost)
+        {
+            NetworkManager.Singleton.StartHost();
+        }
+        else
+        {
+            NetworkManager.Singleton.StartClient();
+        }
+    }
+
+    void CreateRoom()
+    {
+        isHost = true;
+        OnStartSharedSpaceHost?.Invoke();
+        StartSharedSpace();
+    }
+
+    void JoinGameClient()
+    {
+        isHost = false;
+        OnJoinSharedSpaceClient?.Invoke();
+        StartSharedSpace();
+    }
+
+    void StartSharedSpace()
+    {
+        OnStartSharedSpace?.Invoke();
+
+        if(spaceManager.GetColocalizationType() == SharedSpaceManager.ColocalizationType.MockColocalization)
+        {
+            var mockTrackingArgs = ISharedSpaceTrackingOptions.CreateMockTrackingOptions();
+            var roomArgs = ISharedSpaceRoomOptions.CreateLightshipRoomOptions(
+                roomName,
+                capacity: MAX_CLIENTS,
+                description: "SharedARTestMock"
+            );
+
+            spaceManager.StartSharedSpace(mockTrackingArgs, roomArgs);
+            return;
+        }
+
+        if (spaceManager.GetColocalizationType() == SharedSpaceManager.ColocalizationType.ImageTrackingColocalization)
+        {
+            var imageTrackingArgs = ISharedSpaceTrackingOptions.CreateImageTrackingOptions(targetImage, targetImageSize);
+            var roomArgs = ISharedSpaceRoomOptions.CreateLightshipRoomOptions(
+                roomName,
+                capacity: MAX_CLIENTS,
+                description: "SharedARTest"
+            );
+
+            spaceManager.StartSharedSpace(imageTrackingArgs, roomArgs);
+            return;
+        }
+    }
+
+// Start is called before the first frame update
+void Start()
     {
         
     }
